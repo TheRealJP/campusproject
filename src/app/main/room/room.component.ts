685cc56923a2ab47fb21b92b5a51b69d6416a723
Unknown <Jon_athan71@hotmail.com>
2019-01-02 22:54:44 +0100

    reservations done

--- src/app/main/room/room.component.ts
+++ src/app/main/room/room.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Room} from '../_models/room';
 import {Type} from '../_models/type.enum';
+import {Subscription, timer} from 'rxjs';
 
 @Component({
   selector: 'app-room',
@@ -14,26 +15,38 @@ export class RoomComponent implements OnInit {
   @Input() selectedRoom: string;
   isThisRoomSelected: boolean;
   amountOfHoursBooked: number;
-  value = 0;
-  timerValue: number;
-  roomColor: string;
+  roomColor = 'rgb(255,255,255)';
+  private timerSubscription: Subscription;
 
   constructor() {
   }
 
   ngOnInit() {
+    this.initRoomColor();
     this.amountOfHoursBooked = 1;
   }
 
+  // todo: deselect room at given time
+  //  deselect room when selecting another
+  selectRoom() {
+    if (this.room['naam'] !== this.selectedRoom) {
+      this.isThisRoomSelected = false;
+      return;
+    }
+
+    this.isThisRoomSelected = !this.isThisRoomSelected;
+    console.log('selected room:' + this.room['name']);
+  }
 
   hasSlider(): boolean {
-    return this.room['type'] === Type.cafetaria || this.room['type'] === Type.studielandschap;
+    return this.room['type'] === Type.cafetaria
+      || this.room['type'] === Type.studielandschap;
   }
 
   isBookable(): boolean {
     return this.room['type'] === Type.klaslokaal
       || this.room['type'] === Type.aula
-      || this.room['type'] === Type.vergaderzaal && !this.room['bezet'];
+      || this.room['type'] === Type.vergaderzaal;
   }
 
   onBookingChange(value: number) {
@@ -44,41 +57,48 @@ export class RoomComponent implements OnInit {
   }
 
   bookRoom() {
-    console.log(this.room['bezet']);
     this.room['bezet'] = true;
-    console.log(this.room['bezet']);
+    this.bookingTimer();    // updates background color based on timer subscription values
   }
 
-  selectRoom() {
-    if (this.room.naam !== this.selectedRoom) {
-      this.isThisRoomSelected = false;
-      return;
-    }
-
-    this.isThisRoomSelected = !this.isThisRoomSelected;
-    console.log('selected room:' + this.room['name']);
-  }
-
-
-  getColor() {
-    const percentage = (((this.room['drukte'] - 1) * (100 - 1)) / (this.room['capaciteit'] - 1)) + 1;
-    console.log('roomColor before:' + this.roomColor);
-    console.log('' + percentage);
+  // updates roomColor
+  getColor(value: number, maxValue: number) {
+    // replace range of for example: 1-500 to 1-100
+    const percentage = (((value - 1) * (100 - 1)) / (maxValue - 1)) + 1;
     const r = percentage < 50 ? 255 : Math.floor(255 - (percentage * 2 - 100) * 255 / 100);
     const g = percentage > 50 ? 255 : Math.floor((percentage * 2) * 255 / 100);
-    this.roomColor = 'rgb(' + r + ',' + g + ',0)';
-    console.log('roomColor after:' + this.roomColor);
 
+    // switches places with green and red depending on if its a bookable room
+    // so it starts from red and goes to green or the other way around
+    const redPlace = this.isBookable() ? r : g;
+    const greenPlace = this.isBookable() ? g : r;
+    this.roomColor = 'rgb(' + redPlace + ',' + greenPlace + ',0)';
   }
 
-  // getColor() {
-  //   // 1 is reddish
-  //   // 120 is green
-  //   // switch between time of booking OR slider value
-  //
-  //
-  //   const hue = ((1 - percentage * 120).toString(10));
-  //   this.roomColor = ['hsl(', hue, ',100%,50%)'].join('');
-  //
-  // }
+  private initRoomColor() {
+    if (this.hasSlider()) {
+      this.getColor(this.room['drukte'], this.room['capaciteit']);
+    } else if (this.isBookable() && this.room['bezet']) {
+      console.log('reached');
+      this.roomColor = 'rgb(255,255,255)';
+    }
+  }
+
+  private bookingTimer() {
+    const maxHours = this.amountOfHoursBooked * 3600;
+    console.log('maxhours:' + maxHours);
+    let timeObject = new Date();
+    timeObject = new Date(timeObject.getTime() + maxHours * 10);
+    console.log('next reservation can be made in:' + timeObject);
+
+    // change periodOrScheduler to make it go faster, default should be 1000 == 1s
+    this.timerSubscription = timer(0, 10).subscribe(t => {
+      this.getColor(t, maxHours);
+      if (t >= maxHours) {
+        console.log('maxhours reached:' + maxHours);
+        this.room['bezet'] = false;
+        this.timerSubscription.unsubscribe();
+      }
+    });
+  }
 }
